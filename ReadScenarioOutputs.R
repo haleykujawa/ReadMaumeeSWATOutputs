ad5c0771240c2ed539d28088ae6805d5c0d3819a
kujawa.21 <kujawa.21@OH15GYBS3>
2023-11-14 16:01:13 -0500

    Finalize code to combine and summarize hru outputs
    Move headings as function input so I can use for rch file as well

--- ReadScenarioOutputs.R
+++ ReadScenarioOutputs.R
@@ -1,14 +1,20 @@
 ########### Read Maumee SWAT model HRU outputs ################
-library(here)
+rm(list=ls())
 library(tidyverse)
 
 # assume HRU table in each folder containing a binary column with changed hrus called 'changed_hru'
 
 # Place folder that contains all scenario folders below:
 scenario_dir<-'D:\\Maumee model\\MaumeeScenarios' 
+yrs<-c(2008:2010)
 
 # Place list of scenarios here
-scenario_list<-c('scenario 1','scenario 2')
+scenario_list<-c('baseline','scenario 1','scenario 2')
+
+headers_hru<-c("LULC",  "HRU"     ,  "GIS"  ,"SUB",  "MGT" , "MON"  , "AREAkm2"      ,"ETmm",  
+           "SW_ENDmm"    ,"PERCmm","SURQ_CNTmm" ,"LATQGENmm"    ,"GW_Qmm"    ,"WYLDmm"   ,
+           "QTILEmm" ,"SOLPkg/ha" ,"LATQCNTmm" ,"ORGPkg/ha" ,"SEDPkg/ha", "TNO3kg/ha", 
+           "ORGNkg/ha","NSURQkg/ha",  "PUPkg/ha" ,"P_GWkg/ha",    "P_STRS",   "YLDt/ha","TILEPkg/ha")
 
 # Folder structure should be
 
@@ -26,7 +32,11 @@ scenario_list<-c('scenario 1','scenario 2')
 
 # function to read hru outputs
 
-setwd(paste0(scenario_dir,'\\baseline'))
+
+
+readHRU<-function(wd,headers){
+  
+  setwd(wd)
 
 # HRU output structure
 # line 9 = header column
@@ -36,14 +46,9 @@ tmp<-file('output.hru')
 hru_output<-readLines(tmp, n = -1)
 close(tmp)
 
-hru_file_heading<-hru_output[9] # hru file headings
+file_heading<-hru_output[9] # hru file headings
 
 hru_txt<-hru_output[10:length(hru_output)] # hru data text, remove non-data lines
-
-headers<-c("LULC",  "HRU"     ,  "GIS"  ,"SUB",  "MGT" , "MON"  , "AREAkm2"      ,"ETmm",  
-           "SW_ENDmm"    ,"PERCmm","SURQ_CNTmm" ,"LATQGENmm"    ,"GW_Qmm"    ,"WYLDmm"   ,
-           "QTILEmm" ,"SOLPkg/ha" ,"LATQCNTmm" ,"ORGPkg/ha" ,"SEDPkg/ha", "TNO3kg/ha", 
-           "ORGNkg/ha","NSURQkg/ha",  "PUPkg/ha" ,"P_GWkg/ha",    "P_STRS",   "YLDt/ha","TILEPkg/ha")
 
 hru_output<-tibble(.rows=length(hru_txt))
 
@@ -54,28 +59,140 @@ hru_output<-tibble(.rows=length(hru_txt))
 # readLines output of ONLY data columns (crop anything headers and above)
 # data frame to be filled
 
-extract_columns<-function(col_input,file_heading,txt_in,df){
+start<-1 
+
+for (col_input in headers){
 
 location<-gregexpr(pattern =col_input,file_heading)
-start<-unlist(location[1])
-stop<-start+nchar(col_input)-1
+start<-start # assign to 1 or previous stopping point
+stop<-unlist(location[1])+nchar(col_input)-1
 
-new_col<-substr(txt_in,start,stop)
+new_col<-substr(hru_txt,start,stop)
 
-df<-df %>% 
+hru_output<-hru_output %>% 
   mutate({{col_input}} := new_col)
 
-df
+start<-stop+1
 
-return(df)
+}
+
+return(hru_output)
 
 }
 
 
 
-# I'd rather apply this as a function but don't know how to do so right now
-for (cols in headers){
-  print(cols)
-  hru_output<-extract_columns(cols,hru_file_heading,hru_txt,hru_output)
+hru_output<-c()
+
+for (scen in scenario_list){
   
+  wd<-paste0(scenario_dir,'//',scen)
+  
+  # if it is a scenario, read hru table to filter only changed HRUs
+  
+  if (!grepl('baseline',scen)){
+    
+    
+  hru_table<-read.csv(paste0(wd,'//HRU_Table_OCT2023.txt')) %>% 
+    mutate(changed_hru=1) %>% 
+    select(HRU_GIS,changed_hru)
+  
+  add_df<-readHRU(wd,headers_hru) %>% 
+    
+    # Filter unchanged HRUs
+    mutate(across(!LULC, as.numeric)) %>% # convert everything except lulc to numeric
+    left_join(.,hru_table,by=c("GIS"="HRU_GIS")) %>% 
+    filter(changed_hru==1) %>% 
+    select(-changed_hru) %>% 
+    
+    #add year column, assume monthly data
+    mutate(YR=rep(yrs,each=length(unique(GIS))*12)) %>% 
+    
+    # total P and N
+    mutate(totp=`SOLPkg/ha`+`ORGPkg/ha`+`SEDPkg/ha`+`TILEPkg/ha`+`P_GWkg/ha`,
+           totn=`TNO3kg/ha`+`ORGNkg/ha`+`NSURQkg/ha`) %>% 
+    
+    # remove unneeded variables
+    select(-c(SW_ENDmm, LATQGENmm , GW_Qmm, LATQCNTmm,`P_GWkg/ha`,`YLDt/ha`,`P_STRS`,`PUPkg/ha`)) %>% 
+    
+    gather(variable,value,-LULC,-HRU,-GIS,-SUB,-MGT,-MON,-AREAkm2,-YR) %>% 
+    mutate(scenario=scen) %>% 
+    mutate(value=value*2) #TESTING 
+  
+  
+  hru_output<-rbind(hru_output,add_df)
+  
+  rm(add_df,hru_table) # clear memory 
+  
+  }else{
+    
+    # baseline data
+    
+    add_df<-readHRU(wd,headers_hru) %>% 
+      mutate(across(!LULC, as.numeric)) %>% # convert everything except lulc to numeric
+      
+      #add year column, assume monthly data
+      mutate(YR=rep(yrs,each=length(unique(GIS))*12)) %>% 
+      
+      # total P and N
+      mutate(totp=`SOLPkg/ha`+`ORGPkg/ha`+`SEDPkg/ha`+`TILEPkg/ha`+`P_GWkg/ha`,
+             totn=`TNO3kg/ha`+`ORGNkg/ha`+`NSURQkg/ha`) %>% 
+      
+      # remove unneeded variables
+      select(-c(SW_ENDmm, LATQGENmm , GW_Qmm, LATQCNTmm,`P_GWkg/ha`,`YLDt/ha`,`P_STRS`,`PUPkg/ha`)) %>% 
+      
+      gather(variable,value,-LULC,-HRU,-GIS,-SUB,-MGT,-MON,-AREAkm2,-YR) %>% 
+      mutate(scenario=scen)
+      
+      
+    
+    hru_output<-rbind(hru_output,add_df)
+    
+    rm(add_df)
+    
+    
+  }
+  
+  print(paste(scen, 'loaded'))
 }
+
+
+# calculate percent change for each scenario as compared to baseline
+
+hru_output_yearly <- hru_output %>% 
+  mutate(scenario=factor(scenario)) %>% 
+  group_by(GIS,YR,variable,scenario) %>%
+  summarize(value=sum(value,na.rm=T)) %>% # yearly average of monthly values
+  mutate(percent_change=(value-value[scenario=="baseline"])*100/value[scenario=="baseline"]) 
+
+
+#### HRU plots #########
+
+variable_labs<-c('Sediment yield','Surface soluble P','Total P')
+names(variable_labs)<-c('sedyld_change','surqsolp_change','totp_change')
+#,labeller=labeller(variable=variable_labs)
+
+#absolute values
+hru_output_yearly %>% 
+  ggplot(.,aes(x=scenario,y=value))+geom_boxplot()+facet_wrap(~variable,scales='free_y')+
+  xlab("")+ylab("")+
+  # scale_fill_manual(values=c('baseline (2013-2020)'='white', 'land management scenario (2013-2020)'='grey'))+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),
+        panel.background = element_blank(),text = element_text(size = 16),
+        panel.border = element_rect(colour = "black", fill=NA, linewidth=1),
+        legend.title = element_blank())
+
+#percent change
+hru_output_yearly %>% 
+  filter(!(scenario %in% 'baseline')) %>% 
+  ggplot(.,aes(x=scenario,y=percent_change))+geom_boxplot()+facet_wrap(~variable,scales='free_y')+
+  xlab("")+ylab("")+
+  # scale_fill_manual(values=c('baseline (2013-2020)'='white', 'land management scenario (2013-2020)'='grey'))+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),
+        panel.background = element_blank(),text = element_text(size = 16),
+        panel.border = element_rect(colour = "black", fill=NA, linewidth=1),
+        legend.title = element_blank())
+  
+
+  
+
